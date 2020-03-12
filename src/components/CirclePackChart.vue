99c8bba8f0654e59628ab466c7a680e763c11c21
uf0 <scnepa222@gmail.com>
2020-03-12 16:25:36 +0100

    bootstrap circle pack w labels

--- src/components/CirclePackChart.vue
+++ src/components/CirclePackChart.vue
@@ -1,93 +1,166 @@
 <template>
-  <g id="main-chart" ref='mainChart' :transform="'translate('+ x + ',' + y + ')'" :height="height" :width="width">
+  <g
+    id="main-chart"
+    ref="mainChart"
+    :transform="'translate(' + x + ',' + y + ')'"
+    :height="height"
+    :width="width"
+  >
   </g>
 </template>
 
 <script>
-import * as d3 from 'd3'
-import {group} from "d3-array";
+import * as d3 from "d3";
+import { groups } from "d3-array";
 
 export default {
-  name: 'CirclePackChart',
-  props: ['x', 'y', 'height', 'width'],
-  data () {
+  name: "CirclePackChart",
+  props: ["x", "y", "height", "width"],
+  data() {
     return {
       chartData: {},
-      nodeInfo: ''
-    }
+      nodeInfo: ""
+    };
   },
-  mounted () {
-    this.init()
-    this.color = d3.scaleSequential([5, 0], d3.interpolateMagma)
+  mounted() {
+    this.colorScale = d3
+      .scaleOrdinal()
+      .domain(["Poor", "Neutral", "Good"])
+      .range(["#D4003D", "#EAEA6A", "#00E4A2"]);
+
+    this.init();
   },
   methods: {
-    async init () {
-      let csvData = await d3.csv('./data/test-data.csv')
-      // let ] = d3.]().domain(d3.extent(csvData, d => d.lastmonth)).range([10, 500]);
+    async init() {
+      const csvData = await d3.csv("/data/data.csv");
 
       csvData.forEach(el => {
-        el.value = (Math.pow(10,el.lastmonth) / Math.pow(10,10)) * Math.pow(10,8)
-      })
-      let categories = Array.from(group(csvData, d => d.category), ([key, value]) => ({name: key, children: value}))
-      categories.forEach(c => {
-        c.children = Array.from(group(c.children , d => d.second_level_domain), ([key, value]) => ({name: key, children: value}))
-      })
+        el.value =
+          (Math.pow(10, el.lastmonth) / Math.pow(10, 10)) * Math.pow(10, 8);
+      });
 
-      this.chartData = {
-        name: 'chart',
-        children: categories
-      }
+      const heirarchy = groups(
+        csvData,
+        d => d.category,
+        d => d.second_level_domain
+      );
+
+      this.chartData = heirarchy;
     },
-    draw () {
-      let self = this
-      let vHeight = this.height //1000 //this.$el.clientHeight
-      let vWidth = this.width //this.$el.clientWidth
-      let rad = Math.min(vWidth,vHeight)
-      var svg = d3.select(this.$refs.mainChart).attr('width', vWidth).attr('height', vHeight)
-      // Declare d3 layout
+    draw() {
+      let self = this;
+      let vHeight = this.height; //1000 //this.$el.clientHeight
+      let vWidth = this.width; //this.$el.clientWidth
+      let rad = Math.min(vWidth, vHeight);
+      var svg = d3
+        .select(this.$refs.mainChart)
+        .attr("width", vWidth)
+        .attr("height", vHeight);
 
-      d3.select(this.$refs.mainChart).selectAll('*').remove()
+      d3.select(this.$refs.mainChart)
+        .selectAll("*")
+        .remove();
 
-      const root = d3.pack()
-        .size([rad - 10, rad - 10])
-        .padding(5)
-          (d3.hierarchy(this.chartData)
-          .sum(d => d.value)
+      const circle = d3
+        .arc()
+        .innerRadius(0)
+        .outerRadius(d => d)
+        .startAngle(-Math.PI)
+        .endAngle(Math.PI);
+
+      const root = d3
+        .pack()
+        .size([vWidth, vHeight])
+        .padding(10)(
+        d3
+          .hierarchy([null, this.chartData], d =>
+            Array.isArray(d) ? d[1] : undefined
+          )
+          .sum(d => {
+            return d.value;
+          })
           .sort((a, b) => b.value - a.value)
-        )
+      );
 
-      const node = svg.selectAll("g")
-        .data(d3.nest().key(d => d.height).entries(root.descendants()))
-        .join("g")
+      const textScale = d3
+        .scaleLinear()
+        .range([6, 14])
+        .domain(
+          d3.extent(
+            root.descendants().filter(d => d.children && d.children.length > 1),
+            d => d.r
+          )
+        );
+
+      const node = svg
         .selectAll("g")
-        .data(d => d.values)
-        .join("g")
-          .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+        .data(root.descendants())
+        .enter()
+        .append("g")
+        .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
 
-      node.append("circle")
-          .attr("r", d => d.r)
-          .attr("fill", d => this.color(d.height))
-          .on("mouseover", function(d, i) {
-            let el = d3.select(this).attr("fill", '#00f')
-            self.nodeInfo = d.data.name || d.data.hostname
-          })
-          .on("mouseout", function(d, i) {
-            let el = d3.select(this).attr("fill", self.color(d.height))
-            self.nodeInfo = ''
-          })
+      node
+        .append("path")
+        .attr("id", (d, i) => {
+          d.id = "p_" + i;
+          return "p_" + i;
+        })
+        .attr("d", d => circle(d.r));
+
+      const hidden = node.filter(d => d.children && d.children.length == 1);
+
+      hidden
+        .select("path")
+        .attr("stroke", "none")
+        .attr("fill", "none");
+
+      const leaves = node.filter(d => !d.children);
+
+      leaves
+        .select("path")
+        .attr("stroke", "none")
+        .attr("fill", d => this.colorScale(d.data.email_score_name));
+
+      const internal = node.filter(d => d.children && d.children.length > 1);
+
+      internal
+        .select("path")
+        .attr("stroke", "#ddd")
+        .attr("fill", "none");
+
+      internal
+        .append("text")
+        .attr("dy", "-0.3em")
+        .attr("fill", "black")
+        .attr("font-size", d => textScale(d.r) + "px")
+        .attr("text-anchor", "middle")
+        .attr("font-family", "'Arial', sans-serif")
+        .append("textPath")
+        .attr("xlink:href", d => "#" + d.id)
+        .attr("startOffset", "50%")
+        .text(d => {
+          return d.data[0];
+        });
+
+      // .on("mouseover", function(d, i) {
+      //   let el = d3.select(this).attr("fill", '#00f')
+      //   self.nodeInfo = d.data.name || d.data.hostname
+      // })
+      // .on("mouseout", function(d, i) {
+      //   let el = d3.select(this).attr("fill", self.color(d.height))
+      //   self.nodeInfo = ''
+      // })
     }
   },
   watch: {
-    chartData () {
-      this.draw()
+    chartData() {
+      this.draw();
     },
-    height () {
-      this.draw()
+    height() {
+      this.draw();
     }
   }
-}
+};
 </script>
 
-<style scoped lang="stylus">
-
-</style>
+<style scoped lang="stylus"></style>
