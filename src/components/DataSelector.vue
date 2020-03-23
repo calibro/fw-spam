5a091a7d879ae41caee6267993a30220218a627e
Daniele Pezzatini <daniele@bestiario.org>
2020-03-23 12:25:04 +0100

    Load remote file list

--- src/components/DataSelector.vue
+++ src/components/DataSelector.vue
@@ -24,6 +24,9 @@ export default {
       selectedRemote: null,
     }
   },
+  mounted () {
+    this.$store.dispatch('loadRemoteFileList')
+  },
   computed: {
     ...mapState(['remoteFileList'])
   },
