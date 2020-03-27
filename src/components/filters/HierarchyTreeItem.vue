8b6a7a5a74d634e55795f4f70aa8a725e2db48a5
Daniele Pezzatini <daniele@bestiario.org>
2020-03-27 15:05:46 +0100

    Fix issue with min/max in range filters

--- src/components/filters/HierarchyTreeItem.vue
+++ src/components/filters/HierarchyTreeItem.vue
@@ -13,7 +13,7 @@
     </div>
     <transition name="fade">
       <div class="children-container" v-show="openTree">
-        <hierarchy-tree-item ref="childrenList" v-for="(child, index) in children" :item="child" :level="level+1" :excludeNodes="excludeNodes" :onChange="onChange" :key="child.name + '-'+ index" :searchTerm="searchTerm"></hierarchy-tree-item>
+        <hierarchy-tree-item ref="childrenList" v-for="(child, index) in children" :item="child" :level="level+1" :excludeNodes="excludeNodes" :onChange="onChange" :key="child.name + '-'+ index"></hierarchy-tree-item>
       </div>
     </transition>
   </div>
