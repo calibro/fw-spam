2370dbc9d43d20de679d9f2a4a62487365ae666e
uf0 <scnepa222@gmail.com>
2020-04-01 18:44:05 +0200

    electron stuff

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,18 @@ module.exports = {
     process.env.NODE_ENV === "production"
       ? //? `${process.cwd()}/dist/`
         `/fw-spam/`
-      : "/"
+      : "/",
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        productName: "Email reputation",
+        mac: {
+          icon: "build/mac-icon.png"
+        },
+        win: {
+          icon: "build/win-icon.png"
+        }
+      }
+    }
+  }
 };
