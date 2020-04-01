61059d692fd76249d548727dccc0c23117805d0a
uf0 <scnepa222@gmail.com>
2020-04-01 14:56:45 +0200

    starting res electron

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-let url
-if (process.env.NODE_ENV === 'DEV') {
-  url = 'http://localhost:8080/'
+let url;
+if (process.env.NODE_ENV === "DEV") {
+  url = "http://localhost:8080/";
 } else {
-  url = `file://${process.cwd()}/dist/index.html`
+  url = `file://${process.cwd()}/dist/index.html`;
 }
 
-app.on('ready', () => {
-  let window = new BrowserWindow({width: 800, height: 600})
-  window.loadURL(url)
-})
+app.on("ready", () => {
+  let window = new BrowserWindow({ width: 1280, height: 800 });
+  window.loadURL(url);
+});
