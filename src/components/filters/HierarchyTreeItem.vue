bf2be8994eb90c79c82f4315a9683c41b12d0338
uf0 <scnepa222@gmail.com>
2020-04-02 18:37:37 +0200

    text break on firefox

--- src/components/filters/HierarchyTreeItem.vue
+++ src/components/filters/HierarchyTreeItem.vue
@@ -1,83 +1,105 @@
 <template>
-  <div class="tree-item" v-if="item" v-bind:class="{leaf: !isNotLeaf}">
+  <div class="tree-item" v-if="item" v-bind:class="{ leaf: !isNotLeaf }">
     <div class="item-line">
-      <span @click="toggleTree" v-if="isNotLeaf" class="caret" v-bind:class="{open: openTree}"><b-icon-chevron-up /></span>
-      <input type="checkbox"
+      <span
+        @click="toggleTree"
+        v-if="isNotLeaf"
+        class="caret"
+        v-bind:class="{ open: openTree }"
+        ><b-icon-chevron-down
+      /></span>
+      <input
+        type="checkbox"
         :checked="isChecked"
         :name="label"
         class="item-checkbox"
         @click="toggleCheckbox"
         v-indeterminate="isIndeterminate"
         :disabled="isDisabled"
-        />
-      {{label}}
+      />
+      <div class="item-line-label">
+        {{ label }}
+      </div>
     </div>
     <transition name="fade">
       <div class="children-container" v-show="openTree">
-        <hierarchy-tree-item ref="childrenList" v-for="(child, index) in children" :item="child" :level="level+1" :excludeNodes="excludeNodes" :onChange="onChange" :key="child.name + child.ip" :isFiltered="isFiltered"></hierarchy-tree-item>
+        <hierarchy-tree-item
+          ref="childrenList"
+          v-for="(child, index) in children"
+          :item="child"
+          :level="level + 1"
+          :excludeNodes="excludeNodes"
+          :onChange="onChange"
+          :key="child.name + child.ip"
+          :isFiltered="isFiltered"
+        ></hierarchy-tree-item>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
-import Vue from 'vue'
+import { mapState, mapMutations } from "vuex";
+import Vue from "vue";
 
 export default {
-  name: 'HierarchyTreeItem',
-  props: ['item', 'level', 'excludeNodes', 'isFiltered', 'onChange'],
-  data () {
-    return {
-      openTree: this.isFiltered,
-    }
+  name: "HierarchyTreeItem",
+  props: ["item", "level", "excludeNodes", "isFiltered", "onChange"],
+  data() {
+    return {
+      openTree: this.isFiltered
+    };
   },
   computed: {
-    isNotLeaf () {
-      return this.item.level != 'hostname'
+    isNotLeaf() {
+      return this.item.level != "hostname";
     },
-    label () {
-      return this.item.name
+    label() {
+      return this.item.name;
     },
-    children () {
-      let c = this.item.filteredChildren || this.item.children
-      return c ? c.sort((a,b) =>  a.name > b.name ? 1 : -1) : c
+    children() {
+      let c = this.item.filteredChildren || this.item.children;
+      return c ? c.sort((a, b) => (a.name > b.name ? 1 : -1)) : c;
     },
-    isChecked () {
-      return this.$store.getters['data/isNodeChecked'](this.item)
+    isChecked() {
+      return this.$store.getters["data/isNodeChecked"](this.item);
     },
-    isIndeterminate () {
-      return this.$store.getters['data/isNodeIndeterminate'](this.item)
+    isIndeterminate() {
+      return this.$store.getters["data/isNodeIndeterminate"](this.item);
     },
-    isDisabled () {
-      return this.isNotLeaf && this.isFiltered && this.item.filteredChildren.length == 0
+    isDisabled() {
+      return (
+        this.isNotLeaf &&
+        this.isFiltered &&
+        this.item.filteredChildren.length == 0
+      );
     }
   },
   methods: {
-    toggleTree () {
+    toggleTree() {
       if (!this.isDisabled) {
-        this.openTree = !this.openTree
+        this.openTree = !this.openTree;
       }
     },
-    toggleCheckbox (evt) {
-      let val = evt.target.checked
-      this.onChange(this, val)
-    },
+    toggleCheckbox(evt) {
+      let val = evt.target.checked;
+      this.onChange(this, val);
+    }
   },
   watch: {
     isFiltered(newVal, oldVal) {
       // Force open tree when start searching nodes
-      if(!oldVal && newVal) {
-        this.openTree = true
+      if (!oldVal && newVal) {
+        this.openTree = true;
       }
     }
   },
   directives: {
     indeterminate: function(el, binding) {
-      el.indeterminate = Boolean(binding.value)
+      el.indeterminate = Boolean(binding.value);
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
@@ -86,6 +108,10 @@ export default {
     display flex
     margin 0 5px
     align-items: center
+    .item-line-label
+      word-break: break-all
+      line-height: 1
+      margin: 5px 0px
     .item-checkbox
       margin-right 5px
   .children-container
@@ -95,7 +121,7 @@ export default {
     transition transform 0.2s
     margin-right 5px
     &.open
-      transform rotate(180deg)
+      transform rotate(-180deg)
   &.leaf
     margin-left 30px
 
